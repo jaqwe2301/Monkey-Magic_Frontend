99f00bd535493034f727ebf1de6748e489329fdf
jaqwe2301 <jaqwe2301@naver.com>
2025-01-05 23:52:34 +0900

    Feat: 예매 번호 인증 및 투표

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  distDir: "./.next",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,5 @@
-"use client";
+import VotingPage from "./components/Vote";
 
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:8000");
-
-// 투표 데이터 타입 정의
-interface Vote {
-  option: string;
-}
-
-export default function VotingPage() {
-  const [votes, setVotes] = useState<Vote[]>([]); // 타입 명시
-
-  useEffect(() => {
-    socket.on("update", (data: Vote) => {
-      setVotes((prev) => [...prev, data]);
-    });
-
-    return () => {
-      socket.off("update");
-    };
-  }, []);
-
-  const handleVote = (option: string) => {
-    socket.emit("vote", { option });
-  };
-
-  return (
-    <div>
-      <h1>실시간 투표</h1>
-      <button onClick={() => handleVote("Option A")}>Option A</button>
-      <button onClick={() => handleVote("Option B")}>Option B</button>
-      <div>
-        <h2>투표 현황:</h2>
-        {votes.map((vote, index) => (
-          <div key={index}>{vote.option}</div>
-        ))}
-      </div>
-    </div>
-  );
+export default function Main() {
+  return <VotingPage />;
 }
